254cde1603715d92e5a6abd29942eb8c24d3c534
Nick Costa <costa_n1@denison.edu>
2015-04-02 05:22:52 -0400

    Add mobile config file and icons

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Denison Bandersnatch',
+  description: 'An Android app to digitize the ordering process of the Bandersnatch Cafe',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/icons/icon-ldpi.png',
+  'android_mdpi': 'public/icons/icon-mdpi.png',
+  'android_hdpi': 'public/icons/icon-hdpi.png',
+  'android_xhdpi': 'public/icons/icon-xhdpi.png'
+});
