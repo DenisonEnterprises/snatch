68d2ba35796cd3f796ef9005362865629b996b2b
Nick Costa <costa_n1@denison.edu>
2015-04-08 20:36:30 -0400

    Fixed Order List Screen

--- client/employee/backScreen/orderList.js
+++ client/employee/backScreen/orderList.js
@@ -8,31 +8,61 @@ Template.orderList.events({
   },
 });
 
+
+
 Template.itemInfo.helpers({
 	'CheesePizzaNum' : function(){
 		var j = 0;			// number of cheese pizza bagels
-		j = ActiveOrders.find({item: 'Pizza Bagel (Cheese) '}).count();
+		var itemDeets;		// array for the items to fall into
+		ActiveOrders.find().forEach(function(order){ itemDeets = order.item.split('\n'); 
+		for(index = 0; index < itemDeets.length; index++){
+			if(itemDeets[index] == 'Pizza Bagel (Cheese) '){
+				j++;
+			}
+		}
+		});
 		return j;
 	},
 	
 	'pepPizzaNum' : function(){ 
 		var j = 0;			// number of pepperoni pizza bagels
-		j = ActiveOrders.find({item: 'Pizza Bagel (Pepperoni) '}).count()
-		return j;
-	},
+		var itemDeets;		// array for the items to fall into
+		ActiveOrders.find().forEach(function(order){ itemDeets = order.item.split('\n'); 
+		for(index = 0; index < itemDeets.length; index++){
+			if(itemDeets[index] == 'Pizza Bagel (Pep) '){
+				j++;
+			}
+		}
+		});
+		return j;	},
 	
 	'snagelNum' : function(){
 		var j = 0;			// number of snagels
-		j = ActiveOrders.find({item: "Snagel "}).count(); 
-		return j;
-	},
+		var itemDeets;		// array for the items to fall into
+		ActiveOrders.find().forEach(function(order){ itemDeets = order.item.split('\n'); 
+		for(index = 0; index < itemDeets.length; index++){
+			if(itemDeets[index] == 'Snagel '){
+				j++;
+			}
+		}
+		});
+		return j;	},
 	
 	'onShakes' : function(){
 		var j = 0;			// number of oreo nutella milkshakes
-		j = ActiveOrders.find({item: "Shake:  \n"}).count(); 		
-		return j;	
-	}
+		var itemDeets;		// array for the items to fall into
+		var oreo; 		// bool value for milkshake with oreos
+		ActiveOrders.find().forEach(function(order){ itemDeets = order.item.split('\n'); 
+		for(index = 0; index < itemDeets.length; index++){
+			console.log(itemDeets[index]);
+			if(itemDeets[index] == "Shake: "){
+				j++;
+			}
+		}
+		});
+		return j;	}	
 });
+
 
 /*
 Things to do: 
