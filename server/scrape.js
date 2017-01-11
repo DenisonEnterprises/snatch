ffc3a8d5cccebfb5566c9010049dcbe54c6c9ac1
Caileigh Marshall <chm_dance@yahoo.com>
2017-01-10 22:57:53 -0600

    Set chron job times to work with UTC time

--- server/scrape.js
+++ server/scrape.js
@@ -1,15 +1,25 @@
 
 var later = Npm.require('later');
+/*var moment = Npm.require("moment-timezone");
+
+var timezone = "America/Kentucky/Louisville";
+var start_date = new Date();
+
+var zone = moment.tz.zone(timezone);
+var offset = zone.offset(start_date);
+
+var now = new moment(start_date);
+now.subtract(offset, "minutes"); */
 
 later.date.localTime();
+
 var psr = later.parse.recur().every(1).minute();
-// var psr = later.parse.recur().on(3).hour();
 
 SyncedCron.add({
   name: 'Turn app off',
   schedule: function(psr) {
     // parser is a later.parse object
-	  return later.parse.recur().on(2).hour();		// turns off  at 3am every morning 
+	  return later.parse.recur().on(7).hour();		// turns off  at 2am every morning -- UTC 7am
   },
   job: function() {
 	  Meteor.call('appOff');
@@ -20,7 +30,7 @@ SyncedCron.add({
   name: 'Send Email',
   schedule: function(psr) {
     // parser is a later.parse object
-	  return later.parse.recur().on(3).hour();		// sends email at 3am every morning
+	  return later.parse.recur().on(8).hour();		// sends email at 3am every morning -- UTC 8am
 	  },
   job: function() {
 	Meteor.call('sendEmail')
@@ -33,7 +43,7 @@ SyncedCron.add({
   name: 'Turn app on',
   schedule: function(psr) {
     // parser is a later.parse object
-	  return later.parse.recur().on(21).hour();		// turns on at 9pm every evening 
+	  return later.parse.recur().on(2).hour();		// turns on at 9pm every evening -- UTC 2am
   },
   job: function() {
 	// turn the app on and reset order numbers
