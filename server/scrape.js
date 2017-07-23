23bd3d62692ca10fbc8d2a484d6ef9d6e0fa11ee
Caileigh Marshall <chm_dance@yahoo.com>
2017-07-23 00:04:24 -0500

    Fixed cron job times

--- server/scrape.js
+++ server/scrape.js
@@ -1,25 +1,33 @@
 
 var later = Npm.require('later');
-/*var moment = Npm.require("moment-timezone");
 
-var timezone = "America/Kentucky/Louisville";
-var start_date = new Date();
-
-var zone = moment.tz.zone(timezone);
-var offset = zone.offset(start_date);
-
-var now = new moment(start_date);
-now.subtract(offset, "minutes"); */
-
-later.date.localTime();
+later.date.UTC();
 
 var psr = later.parse.recur().every(1).minute();
+
+SyncedCron.add({
+  name: 'Turn app on',
+  schedule: function(psr) {
+    // parser is a later.parse object
+	  return later.parse.recur().on(1).hour();		// turns on at 9pm every evening -- UTC 1am (spring/summer)
+  },
+  job: function() {
+	// turn the app on and reset order numbers
+	Meteor.call('appOn');
+	var app = Instance.findOne({name: 'bandersnatch'});
+	if(app.status == 'on'){
+		Meteor.call('resetOrNum');	
+	}
+	
+	// adding comment to reboot server
+  }
+});
 
 SyncedCron.add({
   name: 'Turn app off',
   schedule: function(psr) {
     // parser is a later.parse object
-	  return later.parse.recur().on(7).hour();		// turns off  at 2am every morning -- UTC 7am
+	  return later.parse.recur().on(6).hour();		// turns off  at 2am every morning -- UTC 6am (spring/summer)
   },
   job: function() {
 	  Meteor.call('appOff');
@@ -30,29 +38,11 @@ SyncedCron.add({
   name: 'Send Email',
   schedule: function(psr) {
     // parser is a later.parse object
-	  return later.parse.recur().on(8).hour();		// sends email at 3am every morning -- UTC 8am
+	  return later.parse.recur().on(7).hour();		// sends email at 3am every morning -- UTC 7am (spring/summer)
 	  },
   job: function() {
 	Meteor.call('sendEmail')
 	Meteor.call('pushFinished')
-	// adding comment to reboot server
-  }
-});
-
-SyncedCron.add({
-  name: 'Turn app on',
-  schedule: function(psr) {
-    // parser is a later.parse object
-	  return later.parse.recur().on(2).hour();		// turns on at 9pm every evening -- UTC 2am
-  },
-  job: function() {
-	// turn the app on and reset order numbers
-	Meteor.call('appOn');
-	var app = Instance.findOne({name: 'bandersnatch'});
-	if(app.status == 'on'){
-		Meteor.call('resetOrNum');	
-	}
-	
 	// adding comment to reboot server
   }
 });
